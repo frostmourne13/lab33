a42eca27be979d455f97d7cbc931eecfe0cb96ae
Serhiy <ivanochko@itstep.org>
2015-12-01 20:43:53 +0200

    Money

---
+++ Money/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money
+{
+    public class MoneyClass
+    {
+        string Name;
+        int value;
+        public MoneyClass(string _Name, int _value) { this.Name = _Name; this.value = _value; }
+    }
+}
